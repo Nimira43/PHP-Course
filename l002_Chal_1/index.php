1e890d2d92aaad9ad5eaf0d2ba6e572f7b53aa0c
Nimira43 <n.rabone72@aol.com>
2024-09-25 23:35:20 +0100

    Completed Variable's challenge.

--- l002_Chal_1/index.php
+++ l002_Chal_1/index.php
@@ -1,7 +1,12 @@
-<php?=
-  $title='Introduction to PHP';
-
-  ?>
+<?php
+  $title = 'Introduction to PHP';
+  $author = 'John Doe';
+  $body = 'PHP (Hypertext Preprocessor) is a widely used server-side scripting
+        language that has revolutionized web development. With its simplicity,
+        flexibility, and vast community support, PHP has become the backbone of
+        countless dynamic websites and web applications.';
+  $pageTitle = 'Brad\'s PHP Blog | ';
+?>
 
   <!DOCTYPE html>
   <html lang="en">
@@ -9,19 +14,14 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Brad's PHP Blog | Introduction to PHP</title>
+    <title><?= $pageTitle . $title; ?></title>
   </head>
 
   <body>
     <main>
-      <h1></h1>
-      <p>By: John Doe</p>
-      <p>
-        PHP (Hypertext Preprocessor) is a widely used server-side scripting
-        language that has revolutionized web development. With its simplicity,
-        flexibility, and vast community support, PHP has become the backbone of
-        countless dynamic websites and web applications.
-      </p>
+      <h1><?= $title; ?></h1>
+      <p>By: <?= $author ?></p>
+      <p><?= $body ?></p>
     </main>
   </body>
 
